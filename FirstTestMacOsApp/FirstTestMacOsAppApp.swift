373c12a4ef59789a5fc01c7dd5d6e714b1b77389
LouGabian <jonathangusse@gmail.com>
2024-10-22 19:09:38 +0200

    Initial Commit

---
+++ FirstTestMacOsApp/FirstTestMacOsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTestMacOsAppApp.swift
+//  FirstTestMacOsApp
+//
+//  Created by Jonathan Gusse on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstTestMacOsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
