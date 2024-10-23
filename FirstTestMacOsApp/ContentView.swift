cbfd820c34aceb9f8e8190807dcf8cf19ee7a210
LouGabian <jonathangusse@gmail.com>
2024-10-23 09:23:39 +0200

    Updating and resolve VideoURL issue

--- FirstTestMacOsApp/ContentView.swift
+++ FirstTestMacOsApp/ContentView.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import AVKit
+
+
 
 struct Option: Hashable {
     let title: String
@@ -16,28 +19,93 @@ struct Option: Hashable {
 
 
 struct ContentView: View {
+    
+    let options: [Option] = [
+        .init(title: "Home", imageName: "house"),
+        .init(title: "About", imageName: "info.circle"),
+        .init(title: "Setting", imageName: "gear"),
+        .init(title: "Social", imageName: "message"),
+        ]
+    
     var body: some View {
+        
         NavigationView {
-            ListView()
+            
+            ListView(options: options)
             
             MainView()
-        }
-    }
-}
+            
+        }//END NavigationView
+        
+    }//END body: some.. ....
+}//END struct... ......
 
 
 struct ListView: View {
+    let options: [Option]
+    
     var body: some View {
-        Text("List")
-    }
-}
+        VStack {
+            ForEach(options, id: \.self) { option in
+                HStack {
+                    Image(systemName: option.imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 15)
+                    
+                    Text(option.title)
+                    
+                    Spacer()
+                }//END HStack.. ...
+                .padding(.top, 10)
+                .padding(.leading, 10)
+                
+            }//END ForEach.. ...
+            
+            Spacer()
+            
+        }//END Vstack.. ...
+        
+        
+        
+    }//END body: some.. ....
+}//END struct... ......
 
 
 struct MainView: View {
+    
+    let cols: [GridItem] = [
+        .init(.flexible()),
+        .init(.flexible()),
+        .init(.flexible())
+    ]
+    
+    let videos = Array(1...6).map { "Video\($0)" } // Vidéos nommées "video1", "video2", etc.
+    
     var body: some View {
-        Text("Main")
-    }
-}
+        VStack {
+            Image("header")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            LazyVGrid(columns: cols) {
+                ForEach(videos, id: \.self) { video in
+                    if let videoURL = Bundle.main.url(forResource: video, withExtension: "mov") {
+                        VideoPlayer(player: AVPlayer(url: videoURL))
+                            .frame(height: 100)
+                    } else {
+                        Text("Vidéo introuvable")
+                            .frame(height: 100)
+                    }
+                    
+                }//END ForEach
+                
+            }//END LazyVgrid.. ......
+            
+        }//END VStack.. ......
+        
+    }//END body: some.. ....
+}//END struct... ......
 
 
 
